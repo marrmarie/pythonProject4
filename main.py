61b12dd3dfab625ec381591c0bbba10d7e649753
marrmarie <mashusy2008@gmail.com>
2023-12-18 02:18:18 +0300

    попытка сделать клик

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import cv2
 import autopy
 import mediapipe as mp
+import math
+import time
+import numpy as np
+import pyautogui
+def cl(result):
+    x8 = result.multi_hand_landmarks[0].landmark[8].x
+    y8 = result.multi_hand_landmarks[0].landmark[8].y
+    x4 = result.multi_hand_landmarks[0].landmark[4].x
+    y4 = result.multi_hand_landmarks[0].landmark[4].y
+    s48 = math.hypot(x8 - x4, y8 - y4)
+
+
 
 cap = cv2.VideoCapture(0)
 width, height = autopy.screen.size()
@@ -23,8 +35,12 @@ while True:
             if id == 8:
                 cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
                 autopy.mouse.move(cx * width / w, cy * height / h)
-
+                if cl(result):
+                    pyautogui.click()
+                    print(4)
+                    time.sleep(3)
         mpDraw.draw_landmarks(img, result.multi_hand_landmarks[0], mp.solutions.hands.HAND_CONNECTIONS)
+    #img = np.fliplr(img)
     cv2.imshow('Handtrack', img)
     cv2.waitKey(1)
 
