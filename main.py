cec3cc69db28f7b112b49f5392d850df913457f5
marrmarie <mashusy2008@gmail.com>
2023-12-18 01:34:45 +0300

    222

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ hands = mp.solutions.hands.Hands(static_image_mode=False,
                          min_tracking_confidence=0.5,
                          min_detection_confidence=0.5)
 
-
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     _, img = cap.read()
@@ -17,7 +17,9 @@ while True:
     if result.multi_hand_landmarks:
         for id, lm in enumerate(result.multi_hand_landmarks[0].landmark):
             h, w, _ = img.shape
-            
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx, cy), 3, (355, 0, 255))
+        mpDraw.draw_landmarks(img, result.multi_hand_landmarks[0], mp.solutions.hands.HAND_CONNECTIONS)
     cv2.imshow('Handtrack', img)
     cv2.waitKey(1)
 
