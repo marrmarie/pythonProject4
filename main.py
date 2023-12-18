64b852e674ab517cb7a8bd4c1a06223696fd692d
marrmarie <mashusy2008@gmail.com>
2023-12-18 13:56:34 +0300

    недоделанный клик + исправленные баги с движением мышки

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import autopy
 import mediapipe as mp
 import math
+import numpy as np
 import pyautogui
 def cl(result):
     x8 = result.multi_hand_landmarks[0].landmark[8].x
@@ -34,11 +35,11 @@ width, height = autopy.screen.size()
 
 hands = mp.solutions.hands.Hands(static_image_mode=False,
                          max_num_hands=1,
-                         min_tracking_confidence=0.5,
-                         min_detection_confidence=0.5)
+                         min_tracking_confidence=0.1,
+                         min_detection_confidence=0.1)
 
 mpDraw = mp.solutions.drawing_utils
-
+print(width, height)
 while True:
     _, img = cap.read()
     result = hands.process(img)
@@ -46,17 +47,19 @@ while True:
         for id, lm in enumerate(result.multi_hand_landmarks[0].landmark):
             h, w, _ = img.shape
             cx, cy = int(lm.x * w), int(lm.y * h)
+            print(cx,cy)
             cv2.circle(img, (cx, cy), 3, (355, 0, 255))
             if finger2(result) and id == 8:
                 cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-                autopy.mouse.move((cx * width) / w, (cy * height) / (h + 300))
+                if cx > 15 and cx < width - 180 and cy > 0 and cy < height:
+                    autopy.mouse.move((width - (cx * width) / w), (cy * height) / (h))
 
                 if cl(result) and finger2(result):
                     autopy.mouse.click()
                     print(4)
                     #time.sleep(3)
         mpDraw.draw_landmarks(img, result.multi_hand_landmarks[0], mp.solutions.hands.HAND_CONNECTIONS)
-    #img = np.fliplr(img)
+    img = np.fliplr(img)
     cv2.imshow('Handtrack', img)
     cv2.waitKey(1)
 
